2706e9db6f77b9a085f1afcae9fc66fa91bd5458
Daisuke Takahashi <d.takahashi1811@gmail.com>
2014-09-02 20:42:17 +0900

    solvel 3.14

--- Chapter3/q3_13.ml
+++ Chapter3/q3_13.ml
@@ -1,14 +1,15 @@
 (* 3.13 *)
-(* 
-  let rec pow1 x n = 
-  match n with
-  | 1 -> x
-  | n' -> x * pow1 x (n-1);;
-*)
 
-let rec pow = 
-  fun x n -> 
-    if n = 1 then x 
-    else x * (pow x (n-1));;
-assert ((pow 10 2) = 100);;
+let rec pow = fun n x -> match n with
+| 1 -> x
+| n' -> x * (pow (n'-1) x);;
+
+(* print_int (pow 2 10);; *)
+
+assert ((pow 2 10) = 100);;
+
+let cube x = (pow 3 x);;
+
+assert ((cube 2) = 8);;
+
 
