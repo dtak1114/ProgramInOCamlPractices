e53bbe3e2a1536ed5520f4e325fe240d01d8626a
Daisuke Takahashi <d.takahashi1811@gmail.com>
2014-09-04 16:59:33 +0900

    rename

--- Chapter5/main.ml
+++ Chapter5/main.ml
@@ -1,8 +1,10 @@
-#use "5-1.ml";;
-#use "5-2.ml";;
-#use "5-3.ml";;
-#use "5-4.ml";;
-#use "5-5.ml";;
-#use "5-6.ml";;
+#use "q5-1.ml";;
+#use "q5-2.ml";;
+#use "q5-3.ml";;
+#use "q5-4.ml";;
+#use "q5-5.ml";;
+#use "q5-6.ml";;
+#use "q5-7.ml";;
+#use "q5-8.ml";;
 
 (* #use "sample.ml";; *)
