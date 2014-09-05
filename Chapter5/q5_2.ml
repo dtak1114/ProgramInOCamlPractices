50d4ab708683ecd66976be8a446b0bd6ac6fcd85
Daisuke Takahashi <d.takahashi1811@gmail.com>
2014-09-05 16:24:32 +0900

    solved 5-2

--- Chapter5/q5_2.ml
+++ Chapter5/q5_2.ml
@@ -68,3 +68,85 @@ let rec zip alist blist =
     | [] -> []
     | ha :: resta -> (ha,hb) :: (zip resta restb)
   );;
+
+
+print_endline("5.2.6");;
+
+let unzip l1= 
+  let rec aggregate alist blist l2 = 
+    match l2 with 
+      [] -> (alist,blist)
+      | (a,b) :: rest -> aggregate (alist @ [a]) (blist @ [b]) rest 
+    in
+    aggregate [] [] l1;;
+
+let actual_526 = unzip(zip [2;3;4;5;6;7;8;9;10;11] [true;true;false;true;false;true;false;false;false;true]);;
+let expected_526 = ([2;3;4;5;6;7;8;9;10;11],[true;true;false;true;false;true;false;false;false;true]);;
+
+assert(expected_526 = actual_526);;
+
+print_endline("5.2.7");;
+
+let filter f l1 =
+  let rec intern l2 ret = 
+    match l2 with
+    |[] -> ret
+    |fst :: rest when f (fst) -> intern rest (ret @ [fst]) 
+    |fst :: rest when not(f(fst)) -> intern rest ret 
+    |fst :: [] when f (fst) -> ret @ [fst] 
+
+  in intern l1 [];;
+
+assert( (filter (fun x -> x >0) [-9;0;2;5;-3]) = ([2;5]));;
+
+print_endline("5.2.8");;
+
+let take n l =
+  let rec intern n l ret =
+    if n = 0 then (List.rev ret)
+    else match l with
+       fst :: rest -> intern (n-1) rest (fst::ret) 
+  in intern n l [];;
+
+assert((take 3 [1;2;3;4;5]) = [1;2;3]);;
+
+let drop n l =
+  let rec intern n l ret =
+    if n = 0 then l
+    else match l with
+      fts :: rest -> intern (n-1) rest (fst::ret)
+    in intern n l [];;
+
+assert((drop 3 [1;2;3;4;5]) = [4;5]);;
+
+print_endline("5.2.9");;
+
+let max_list l1 = 
+  let pivot = List.hd l1 in
+  let rec intern l2 max =
+    match l2 with
+    [] ->  max
+    |fst :: rest when fst > max -> intern rest fst
+    |fst :: rest when fst <= max -> intern rest max
+  in intern l1 pivot;;
+
+assert((max_list [7;9;0;-5]) = 9);;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
